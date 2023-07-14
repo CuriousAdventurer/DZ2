f003130bc49953f077c0af8edd3e7d024e572abf
CuriousAdventurer <sworddefender@yandex.ru>
2023-07-14 16:28:08 +0300

    как работает так работает 13я

--- zadacha13/Program.cs
+++ zadacha13/Program.cs
@@ -1,11 +1,31 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.Write("Введите трёхзначное число, чтобы узнать третью цифру этого числа: ");
-int Number = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(Number);
-    if (Number<100)
-    {
-    Console.WriteLine("У этого числа нет третьей цифры");
-    return;
-    }
-  Console.WriteLine("Итоговый результат: "+stringNumber[2]);
+// int Prompt (string conditions) //https://learn.microsoft.com/en-us/dotnet/api/system.speech.synthesis.prompt?view=netframework-4.8.1
+// {
+// Console.Write(conditions);
+// string meaning=Console.ReadLine(); //https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=net-7.0
+// int result=Convert.ToInt32(meaning);
+// return result;
+// }
+//тут проблем не должно быть
+
+ int ThirdNumber(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }
+Console.Write("Введите число, состоящее из трех и более знаков: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+ 
+if (ThirdNumber(number1) == -1)
+Console.WriteLine("Учимся считать до 3х, потом возвращаемся сюда.");
+else
+Console.WriteLine($"третий знак в числе: {ThirdNumber(number1)}");
