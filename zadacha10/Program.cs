ec3b919e56a76d8e8d651ca1cbe6d6ea15d7ed76
CuriousAdventurer <sworddefender@yandex.ru>
2023-07-14 15:40:30 +0300

    peredelka pod ocenku

--- zadacha10/Program.cs
+++ zadacha10/Program.cs
@@ -3,17 +3,28 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.Write("Введите трёхзначное число, чтобы узнать вторую цифру этого числа: ");
-int ThreeNumbersNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(ThreeNumbersNumber);
-    if (ThreeNumbersNumber<100)
-    {
-    Console.WriteLine("Попробуй пересчитать количество цифр, недобор...");
-    return;
-    }
-        if (ThreeNumbersNumber>999)
-        {
-        Console.WriteLine("Попробуй пересчитать количество цифр, перебор...");
-        return;
-        }
-Console.WriteLine("Итоговый результат: "+stringNumber[1]);
+//Отлично (ааааааааааааааа!!!!!): Если решены все задачи (компилируются, выполняются, выдают ожидаемый результат). 
+//Числовые операции по выделению цифр сделаны при помощи операций “/” и “%”.
+
+int Prompt (string conditions) //https://learn.microsoft.com/en-us/dotnet/api/system.speech.synthesis.prompt?view=netframework-4.8.1
+
+{
+System.Console.Write(conditions);
+string meaning=Console.ReadLine(); //https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=net-7.0
+int result=Convert.ToInt32(meaning);
+return result;
+}
+
+int Number = Prompt("Введите трёхзначное число, чтобы узнать вторую цифру этого числа: ");
+if (Number<100)
+{
+Console.WriteLine ("Учись считать до трех, солнышко!");
+return;
+}
+if (Number>999)
+{
+Console.WriteLine ("Учись считать, солнышко!");
+return;
+}
+int secondnumber=Number/10%10;
+Console.WriteLine($"Второе число: "+secondnumber);
